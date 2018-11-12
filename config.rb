6db1543397f1b2894b882e0a3e4391773aaf12a3
SUMITA TAKAHASHI <sumi.5.goo@gmail.com>
2018-11-12 20:37:00 +0900

    finished home and team page

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["sumita", "willem", "min", "luis", "francis", "josh"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
